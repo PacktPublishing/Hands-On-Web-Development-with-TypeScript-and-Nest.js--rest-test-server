08c1932b12edcda55757c9fbe58a83892a909895
Florian GOTO <gotoflorian.pro@gmail.com>
2019-04-17 20:15:20 +0200

    vid1.03 - end

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RestapiModule } from './restapi/restapi.module';
 
 @Module({
-  imports: [],
+  imports: [RestapiModule],
   controllers: [AppController],
   providers: [AppService],
 })
