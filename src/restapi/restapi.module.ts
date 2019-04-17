f23946cdcc63e0f6407899a10b6f5ca6b99a45b6
Florian GOTO <gotoflorian.pro@gmail.com>
2019-04-17 22:43:31 +0200

    vid1.04 - start

--- src/restapi/restapi.module.ts
+++ src/restapi/restapi.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ApiController } from './api/api.controller';
 
-@Module({})
+@Module({
+  controllers: [ApiController]
+})
 export class RestapiModule {}
