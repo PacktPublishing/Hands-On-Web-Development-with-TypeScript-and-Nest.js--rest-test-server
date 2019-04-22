69c1fe5feda584f52f254d3401db8e861c6ad6a9
Florian GOTO <gotoflorian.pro@gmail.com>
2019-04-22 22:05:04 +0200

    vid1.06 - end

--- src/cryptoapi/cryptoapi.module.ts
+++ src/cryptoapi/cryptoapi.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { CryptoapiService } from './cryptoapi.service';
 import { CryptoapiController } from './cryptoapi.controller';
+import { IdentifyMiddleware } from './middlewares/identify.middleware';
 
 @Module({
   controllers: [CryptoapiController],
   providers: [CryptoapiService],
 })
-export class CryptoapiModule {}
+export class CryptoapiModule implements NestModule {
+  // required method to apply middlewares
+  configure(consumer: MiddlewareConsumer) {
+    // ovject with helper methods for managing middlewares
+    consumer
+      // which middlewares to apply (can pass a list of middlewares)
+      .apply(IdentifyMiddleware)
+      // exclude specific requests from passing through the middlewares
+      .exclude(
+        { path: 'cryptoapi', method: RequestMethod.GET },
+        { path: 'cryptoapi', method: RequestMethod.DELETE },
+      )
+      // restrict the middlewares to specific requests
+      .forRoutes(CryptoapiController);
+  }
+}
---
+++ src/cryptoapi/middlewares/identify.middleware.ts
@@ -0,0 +1,16 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { NextFunction } from 'express-serve-static-core';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class IdentifyMiddleware implements NestMiddleware {
+    // required method to implement a middleware
+    use(req: Request, res: Response, next: NextFunction) {
+        // add an uuid to the object on the request body
+        req.body.id = uuid();
+
+        // let the request pass through to the handler
+        next();
+    }
+}
